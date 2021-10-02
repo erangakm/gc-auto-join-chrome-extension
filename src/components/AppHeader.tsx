13fcb0712261aaa2752a4348dfe0abbec5e172af
Eranga Manchanayake <erangakm@gmail.com>
2021-10-03 00:16:01 +1000

    more style improvements

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -4,15 +4,26 @@ import React from "react";
 export const AppHeader: React.FC<{}> = ({ children }) => {
   return (
     <>
-      <Grid container spacing={2}>
+      <Grid container style={{ padding: "8px" }}>
         <Grid item xs={2}>
-          <img width={"50px"} alt="logo" src="./logo64.png"></img>
+          <img width={"40px"} alt="logo" src="./logo64.png"></img>
         </Grid>
         <Grid item xs={10}>
-          <p>Google Calendar assistant</p>
+          <p style={{
+            fontSize: "16px",
+            fontWeight: 800,
+            marginTop: "4%",
+            marginLeft: "-2%"
+          }}>Google Calendar assistant</p>
         </Grid>
       </Grid>
-      {children}
+      <div id="seperator" style={{
+        borderTop: "1px solid #b0b0b06e"
+      }}>
+      </div>
+      <div style={{ padding: "8px", paddingTop: "0px" }}>
+        {children}
+      </div>
     </>
   )
 };
