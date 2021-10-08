f521f84a020458dce8f51b7fe75be994f6d91e24
Eranga Manchanayake <erangakm@gmail.com>
2021-10-08 16:10:20 +1100

    update logo temporarily

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -4,18 +4,22 @@ import { Seperator } from "./Seperator";
 export const AppHeader: React.FC<{}> = ({ children }) => {
   return (
     <>
-      <div className="container p-2" style={{ backgroundColor: "#4285F4"}}>
+      <div className="container p-2 pt-3">
         <div className="row">
           <div className="col text-center">
-            <img className="mr-2" alt="logo" src="./logo64.png" style={{ height: "35px" }}></img>
+            <img className="mr-2" alt="logo" src="./temp64.png" style={{ height: "35px" }}></img>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center">
             <p style={{
-              fontSize: "15px",
-              fontWeight: 800,
-              height: "35px",
-              lineHeight: "35px",
-              position: "relative",
-              top: "4px",
-            }} className="center mb-0 d-inline-block">Video meetings assistant</p>
+                fontSize: "15px",
+                fontWeight: 800,
+                height: "35px",
+                lineHeight: "35px",
+                position: "relative",
+                top: "4px",
+              }} className="center mb-0 d-inline-block">Video meetings assistant</p>
           </div>
         </div>
       </div>
