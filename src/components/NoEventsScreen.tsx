00167fed9bb4d0a2ddb5b5344843d04af148a586
Eranga Manchanayake <erangakm@gmail.com>
2021-10-09 22:11:07 +1100

    add styles to no events screen

--- src/components/NoEventsScreen.tsx
+++ src/components/NoEventsScreen.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 export const NoEventsScreen: React.FC<{}> = () => {
+  const centerStyle: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+
   return (
-    <>
-      <p>You don't have any events with video meetings today.</p>
-    </>
+    <div className="p-2 pt-3">
+      <div className="image" style={centerStyle}>
+        <img src="./empty_calendar.png" alt="no events" style={{ width: "100px" }}></img>
+      </div>
+      <div className="mt-2" style={{
+        ...centerStyle,
+        fontSize: "12px"
+      }}>
+        <p>You don't have any events with video meetings today.</p>
+      </div>
+    </div>
   )
 };
