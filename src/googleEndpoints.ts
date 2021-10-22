e8b760ec12f05ddc8b9cb1ef6e8b380e3ff63c8c
Eranga Manchanayake <erangakm@gmail.com>
2021-10-22 22:22:50 +1100

    add google endpoints

---
+++ src/googleEndpoints.ts
@@ -0,0 +1,6 @@
+export const refreshTokenEndpoint  = "https://oauth2.googleapis.com/token";
+export const oAuthEndpoint         = "https://accounts.google.com/o/oauth2/v2/auth";
+export const emailRequestEndpoint  = "https://www.googleapis.com/oauth2/v3/userinfo";
+export const validateTokenEndpoint = "https://www.googleapis.com/oauth2/v2/tokeninfo";
+export const extensionUrl          = "https://hmomimaiigjfdpbgjdcodnaibladllla.chromiumapp.org/";
+export const calendarEventEndpoint = "https://www.googleapis.com/calendar/v3/calendars/primary/events";
